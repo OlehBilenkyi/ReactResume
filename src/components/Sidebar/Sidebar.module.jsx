efea97f43442e782b96024c6743c4c357a388779
Oleh Bilenkyi <i3eLuy@gmail.com>
2025-05-16 18:38:40 +0200

    Sidebar

---
+++ src/components/Sidebar/Sidebar.module.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Sidebar = styled.aside `
+  background: #e0ebfe;
+  padding: 40px;
+  width: 370px;
+  box-sizing: border-box;
+`
+
+const SidebarContent = styled.div `
+  padding-left: 20px;
+  padding-right: 20px;
+`
