473f8de873d66f839109cf41e690b379ded95cc4
Oleh Bilenkyi <i3eLuy@gmail.com>
2025-05-16 19:48:44 +0200

    my-projects

--- src/components/MainContent/WorkExperience/WorkExperience.jsx
+++ src/components/MainContent/WorkExperience/WorkExperience.jsx
@@ -1,78 +1,62 @@
-import {} from './WorkExperience.module'
+import {
+  WordExperienceContainer,
+  WorkExperienH4,
+  WorkExperienTime,
+  WorkExperionseSeparator,
+  WorkExperionseList,
+  WorkExperionseItems,
+  MyProjectsContainerName,
+  MyProjectsContainer,
+} from "./WorkExperience.module";
+import MyEducation from "./Education/Education";
 
-const Experience = () => (
-            <div class="my-projects-container">
-          <h3 class="my-projects-container-name">Work Experience</h3>
-          <!-- Company 1 -->
-          <div class="word-experience-container">
-            <h4 class="work-experien-h4">
-              Junior JavaScript Developer (Internship)
-            </h4>
-            <p class="work-experien-time">
-              February 2023 - Present
-              <span class="work-experionse-separator">|</span> Remote
-            </p>
-            <ul class="work-experionse-list">
-              <li class="work-experionse-items">
-                Developed and maintained web applications including landing
-                pages, small e-commerce platforms, and mobile apps
-              </li>
-              <li class="work-experionse-items">
-                Integrated payment systems (Stripe API) and worked with various
-                APIs (Google OAuth, Telegram)
-              </li>
-              <li class="work-experionse-items">
-                Optimized performance of web services and managed server
-                configurations
-              </li>
-              <li class="work-experionse-items">
-                Created responsive web components and interactive user
-                interfaces using modern JavaScript
-              </li>
-              <li class="work-experionse-items">
-                Implemented security measures including CSRF protection and
-                Google reCAPTCHA
-              </li>
-              <li class="work-experionse-items">
-                Managed tasks using Trello and followed technical specifications
-                (ToR)
-              </li>
-              <li class="work-experionse-items">
-                Used Git for version control and Composer for PHP dependency
-                management
-              </li>
-            </ul>
-          </div>
-          <!-- Company 2 -->
-          <div class="word-experience-container">
-            <h4 class="work-experien-h4">
-              Electrical Engineer
-              <span class="company-name">(Merchant Fleet)</span>
-            </h4>
-            <p class="work-experien-time">
-              April 2019 - April 2023
-              <span class="work-experionse-separator">|</span> Mykolaiv, Ukraine
-            </p>
-            <ul class="work-experionse-list">
-              <li class="work-experionse-items">
-                Maintained and repaired ship electrical systems under extreme
-                maritime conditions
-              </li>
-              <li class="work-experionse-items">
-                Ensured operational safety of electrical installations and
-                automated control systems
-              </li>
-              <li class="work-experionse-items">
-                Led technical teams of 3-5 engineers during critical maintenance
-                operations
-              </li>
-              <li class="work-experionse-items">
-                Developed emergency response protocols for electrical system
-                failures
-              </li>
-            </ul>
-          </div>
-          <!-- Education -->
-         
-        </div>
-)
+export const Experience = () => {
+  return (
+    <>
+      <MyProjectsContainer>
+        <MyProjectsContainerName>Work Experience</MyProjectsContainerName>
+        <WordExperienceContainer>
+          <WorkExperienH4>
+            Junior JavaScript Developer (Internship)
+          </WorkExperienH4>
+          <WorkExperienTime>
+            February 2023 - Present
+            <WorkExperionseSeparator>|</WorkExperionseSeparator> Remote
+          </WorkExperienTime>
+          <WorkExperionseList>
+            <WorkExperionseItems>
+              Developed and maintained web applications including landing pages,
+              small e-commerce platforms, and mobile apps
+            </WorkExperionseItems>
+            <WorkExperionseItems>
+              Integrated payment systems (Stripe API) and worked with various
+              APIs (Google OAuth, Telegram)
+            </WorkExperionseItems>
+            <WorkExperionseItems>
+              Optimized performance of web services and managed server
+              configurations
+            </WorkExperionseItems>
+            <WorkExperionseItems>
+              Created responsive web components and interactive user interfaces
+              using modern JavaScript
+            </WorkExperionseItems>
+            <WorkExperionseItems>
+              Implemented security measures including CSRF protection and Google
+              reCAPTCHA
+            </WorkExperionseItems>
+            <WorkExperionseItems>
+              Managed tasks using Trello and followed technical specifications
+              (ToR)
+            </WorkExperionseItems>
+            <WorkExperionseItems>
+              Used Git for version control and Composer for PHP dependency
+              management
+            </WorkExperionseItems>
+          </WorkExperionseList>
+        </WordExperienceContainer>
+
+        <MyEducation />
+      </MyProjectsContainer>
+    </>
+  );
+};
--- src/components/MainContent/WorkExperience/WorkExperience.module.jsx
+++ src/components/MainContent/WorkExperience/WorkExperience.module.jsx
@@ -1,2 +1,44 @@
 import styled from 'styled-components'
 
+const WordExperienceContainer = styled.div `
+  margin-bottom: 20px;
+`
+const WorkExperienH4 = styled.h4 `
+  font-weight: 700;
+  font-size: 20px;
+  color: #000;
+  margin-bottom: 10px;
+`
+
+const WorkExperienTime = styled.p  `
+  font-weight: 400;
+  font-size: 14px;
+  color: #a8a8a8;
+  margin-bottom: 10px;
+
+`
+const WorkExperionseSeparator = styled.span `
+  color: #000;
+`
+
+const WorkExperionseList = styled.ul `
+  padding-left: 20px;
+`
+
+const WorkExperionseItems = styled.li `
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 1.6;
+  color: #595959;
+  margin-bottom: 5px;
+`
+const MyProjectsContainerName = styled.h3`
+  font-weight: 700;
+  font-size: 24px;
+  color: #000;
+  margin-bottom: 10px;
+`
+
+const MyProjectsContainer = styled.div`
+  margin-bottom: 40px;
+`
