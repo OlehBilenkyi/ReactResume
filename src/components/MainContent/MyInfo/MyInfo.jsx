a84f8a7838ddceaca587e3aa8a6b174c8a41dd58
Oleh Bilenkyi <i3eLuy@gmail.com>
2025-05-28 19:32:22 +0200

    description

--- src/components/MainContent/MyInfo/MyInfo.jsx
+++ src/components/MainContent/MyInfo/MyInfo.jsx
@@ -1,18 +1,18 @@
 import PropTypes from "prop-types";
 import styles from "./MyInfo.module.scss";
 
-const MyInfo = ({ profession, name, description }) => (
+const MyInfo = ({ profession, name }) => (
   <div className={styles.infoContainer}>
     <h2 className={styles.profession}>{profession}</h2>
     <h1 className={styles.name}>{name}</h1>
-    <p className={styles.textDescr}>{description}</p>
+    
   </div>
 );
 
 MyInfo.propTypes = {
   profession: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  description: PropTypes.string,
+  
 };
 
 export default MyInfo;
--- src/components/Sidebar/Contacts/Contacts.jsx
+++ src/components/Sidebar/Contacts/Contacts.jsx
@@ -1,51 +1,57 @@
-import {
-  ContactConteiner,
-  ContactsLinks,
-  ContactsTitel,
-  ContactsTitle,
-  ContactsItem,
-} from "./Contacts.styled";
+import styles from "./Contacts.module.scss";
 
 const Contacts = () => (
-  <ContactConteiner>
-    <ContactsTitel>Portfolio & Contacts</ContactsTitel>
-    <ContactsTitle>GitHub:</ContactsTitle>
-    <ContactsLinks href="https://github.com/OlehBilenkyi" target="_blank">
-      OlehBilenkyi
-    </ContactsLinks>
-    <ContactsItem>
-      <ContactsTitle>LinkedIn:</ContactsTitle>
-      <ContactsLinks
+  <div className={styles.contactContainer}>
+    <h3 className={styles.contactsTitel}>Portfolio & Contacts</h3>
+    <p className={styles.contactsItem}>
+      <span className={styles.contactsTitle}>GitHub:</span>
+      <a
+        className={styles.contactsLinks}
+        href="https://github.com/OlehBilenkyi"
+        target="_blank"
+      >
+        OlehBilenkyi
+      </a>
+    </p>
+    <p className={styles.contactsItem}>
+      <span className={styles.contactsTitle}>LinkedIn:</span>
+      <a
+        className={styles.contactsLinks}
         href="https://www.linkedin.com/in/oleh-bilenkyi-95a55b177/"
         target="_blank"
       >
         Oleh Bilenkyi
-      </ContactsLinks>
-    </ContactsItem>
-    <ContactsItem>
-      <ContactsTitle>Facebook:</ContactsTitle>
-      <ContactsLinks
+      </a>
+    </p>
+    <p className={styles.contactsItem}>
+      <span className={styles.contactsTitle}>Facebook:</span>
+      <a
+        className={styles.contactsLinks}
         href="https://www.facebook.com/profile.php?id=100005711576476"
         target="_blank"
       >
         Oleh Bilenkyi
-      </ContactsLinks>
-    </ContactsItem>
-    <ContactsItem>
-      <ContactsTitle>Email:</ContactsTitle>
-      <ContactsLinks href="mailto:13eLuy@gmail.com">
+      </a>
+    </p>
+    <p className={styles.contactsItem}>
+      <span className={styles.contactsTitle}>Email:</span>
+      <a className={styles.contactsLinks} href="mailto:13eLuy@gmail.com">
         I3eLuy@gmail.com
-      </ContactsLinks>
-    </ContactsItem>
-    <ContactsItem></ContactsItem>
-    <ContactsItem>
-      <ContactsTitle>Telegram:</ContactsTitle>
-      <ContactsLinks href="https://t.me/OJIeja">@OJIeja</ContactsLinks>
-    </ContactsItem>
-    <ContactsItem>
-      <ContactsTitle>Phone:</ContactsTitle>
-      <ContactsLinks href="tel:+380985745049">+380985745049</ContactsLinks>
-    </ContactsItem>
-  </ContactConteiner>
+      </a>
+    </p>
+    <p className={styles.contactsItem}>
+      <span className={styles.contactsTitle}>Telegram:</span>
+      <a className={styles.contactsLinks} href="https://t.me/OJIeja">
+        @OJIeja
+      </a>
+    </p>
+    <p className={styles.contactsItem}>
+      <span className={styles.contactsTitle}>Phone:</span>
+      <a className={styles.contactsLinks} href="tel:+380985745049">
+        +380985745049
+      </a>
+    </p>
+  </div>
 );
+
 export default Contacts;
