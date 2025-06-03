c4b1da0187e554afac2cc7734e58aa644d3d1bb1
Oleh Bilenkyi <i3eLuy@gmail.com>
2025-06-03 23:09:14 +0200

    SoftSkills.module

--- src/components/Sidebar/Languages/Languages.tsx
+++ src/components/Sidebar/Languages/Languages.tsx
@@ -1,14 +1,14 @@
-import styles from "../ContactsLanguagesSummaryTechSkills.module.scss";
+import styles from "./Languages.module.scss";
 
 const Languages = () => (
   <div className={styles.skillsContainer}>
     <h3 className={styles.skillsTitle}>Languages</h3>
-    <h4 className={styles.skillsList}>
+    <div className={styles.skillsList}>
       <p className={styles.skillsListItem}>Ukrainian - Native</p>
       <p className={styles.skillsListItem}>Polish: A2 (Basic)</p>
       <p className={styles.skillsListItem}>English - A2</p>
       <p className={styles.skillsListItem}>Russian - C2</p>
-    </h4>
+    </div>
   </div>
 );
 
