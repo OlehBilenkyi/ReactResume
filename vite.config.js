fd8d34e7bc14492532ffa21cd141d50dcbf8c191
Oleh Bilenkyi <i3eLuy@gmail.com>
2025-06-25 01:07:58 +0200

    "predeploy": "npm run build",     "deploy": "gh-pages -d dist",

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ReactResume/", // Add this line for GitHub Pages
   optimizeDeps: {
     include: ["rxjs/internal/BehaviorSubject"],
   },
